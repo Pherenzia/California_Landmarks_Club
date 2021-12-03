5cce0275886dcb6c52ecd5e2ed3ce996aa936257
Michael Orr <mike@glquote.com>
2021-12-03 13:46:52 -0800

    users and landmarks routes working in insomnia

--- server/router/nonAuthRouter.js
+++ server/router/nonAuthRouter.js
@@ -7,30 +7,35 @@ const {
   CommentComment,
 } = require("../models");
 
-router.get("/", (req, res) => {
-  res.json();
-});
 
 router.get("/landmarks", (req, res) => {
-  
-  res.json();
+  Landmark.find({}, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 router.get("/landmarks/:id", (req, res) => {
-  res.json();
-});
-
-router.get("/users", (req, res) => {
-  res.json();
-});
-
-router.get("/users/:id", (req, res) => {
-  res.json();
-});
-
-router.post("/posts", (req, res) => {
-    
+  Landmark.findOne({ _id: req.params.id }, (err, items) => {
+    console.log(items)
+    if (err) res.status(500).send(error);
+    res.status(200).json(items);
   });
+});
 
+router.get("/users", async (req, res) => {
+  const users = await User.find()
+  res.json(users);
+});
+
+router.get("/users/:id", async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  res.json(user);
+});
+
+router.post("/posts", (req, res) => {});
 
 module.exports = router;
